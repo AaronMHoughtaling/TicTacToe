c2dade8c9132865c4bc904c01085e2edb0c1e947
Aaron <houghtalinga1@gmail.com>
2019-07-24 23:35:36 -0400

    Added Correct Files
    Beginning of TicTacToe

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public int user = 0;
+        public int turns = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Content == null)
+            {
+                //CheckWinner();
+                if (user % 2 == 0 && turns < 10)
+                {
+                    button.Content = "X";
+                    user++;
+                    turns++;
+                }
+                else
+                {
+                    button.Content = "O";
+                    user++;
+                    turns++;
+                }
+            }
+        }
+    }
+}
